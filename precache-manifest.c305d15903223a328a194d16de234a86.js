4ff0e3abc20214244e430a7a7fb327385b017d49
piet0024 <n.pietrantonio0024@live.com>
2019-01-25 16:23:28 -0500

    Updates

---
+++ precache-manifest.c305d15903223a328a194d16de234a86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f800456919005af430e",
+    "url": "/gh-pages-test/static/js/runtime~main.3f800456.js"
+  },
+  {
+    "revision": "c9cb4a930ba0a3bcc8af",
+    "url": "/gh-pages-test/static/js/main.c9cb4a93.chunk.js"
+  },
+  {
+    "revision": "0f04da893ab551051ea2",
+    "url": "/gh-pages-test/static/js/1.0f04da89.chunk.js"
+  },
+  {
+    "revision": "c9cb4a930ba0a3bcc8af",
+    "url": "/gh-pages-test/static/css/main.6fa8222e.chunk.css"
+  },
+  {
+    "revision": "0f04da893ab551051ea2",
+    "url": "/gh-pages-test/static/css/1.39f6896e.chunk.css"
+  },
+  {
+    "revision": "6cad3cf704ed25b91b2025bb3f28b90a",
+    "url": "/gh-pages-test/index.html"
+  }
+];
